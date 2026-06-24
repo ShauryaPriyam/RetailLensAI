176b4dbbf0a05d0318ea149e8d7d240ff2b302b3
Shaurya <promispriyam04@gmail.com>
2026-06-24 11:22:32 +0530

    Fixed bugs

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://retail-lens-ai.vercel.app/"
+    "https://retail-lens-ai.vercel.app"
 ]
 
 app.add_middleware(
